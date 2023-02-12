2d77d5339406e374589709f53a1924dc7b5ff1fc
olgapro <protasolga2807@gmail.com>
2023-02-13 00:12:45 +0200

    HW_4

--- ta/src/main/java/com/softserve/edu/hw4/App.java
+++ ta/src/main/java/com/softserve/edu/hw4/App.java
@@ -1,2 +1,33 @@
-package com.softserve.edu.hw4;public class App {
+package com.softserve.edu.hw4;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int temp = 0;
+
+        System.out.println("How many numbers do you want to compare: ");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] mass = new int [n];
+
+        System.out.println("Enter " + n + " numbers via Enter: ");
+        for(int i=0; i<mass.length; i++){
+            mass[i] = sc.nextInt();
+        }
+
+        for (int i=0; i<mass.length; i++){
+            for(int j=i+1; j<mass.length; j++){
+                if(mass[i]>mass[j]){
+                    temp = mass[i];
+                    mass[i] = mass[j];
+                    mass[j] = temp;
+                }
+                //
+            }
+        }
+        for (int i=0;i<mass.length;i++)
+            System.out.print(mass[i]+" ");
+    }
 }
